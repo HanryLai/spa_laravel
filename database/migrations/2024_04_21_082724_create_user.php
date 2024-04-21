80202b9abf6cd98f47678bf82aaab9e85bc46367
Hieu Lai <ldmhieudev@gmail.com>
2024-04-21 15:36:18 +0700

    tableuser,admin,customer,staff

---
+++ database/migrations/2024_04_21_082724_create_user.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->string('id',100)->primary();
+            $table->string('username',32);
+            $table->string('email',15);
+            $table->string('phone',15);
+            $table->string('role',8);
+            $table->timestamp('login_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
