5ed8ebabb59cb325b379732d086e54dffdc4fe3e
Hieu Lai <ldmhieudev@gmail.com>
2024-04-21 16:11:30 +0700

    migration blog, order-detail

---
+++ database/migrations/2024_04_21_090838_create_order_detail.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_detail', function (Blueprint $table) {
+            $table->string('order_id',100)->primary();
+            $table-> string('product_id',100)->nullable();
+            $table->string('combo_product_id',100)->nullable();
+            $table->unsignedInteger('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_detail');
+    }
+};
