8e48083888b5e9352a2b0a78e3842717420f56cb
Hieu Lai <ldmhieudev@gmail.com>
2024-04-21 16:14:53 +0700

    product,combo_product

---
+++ database/migrations/2024_04_21_091309_create_combo_product.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('combo_product', function (Blueprint $table) {
+            $table->string('id',100)->primary()->unique();
+            $table->string('name',50);
+            $table->longText('description');
+            $table->double('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('combo_product');
+    }
+};
